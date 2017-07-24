7f0dbb3c46b4e118dda312eb6190c38ab2474f36
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-07-24 18:50:26 +0300

    Вывод приветсвия или формы на экран

--- start/form.php
+++ start/form.php
@@ -1,10 +1,19 @@
 
 <?php
+// вывести на экран приветсвие, если форма передана на обработку
+if ($_POST['user']) {
+  print "Hello, ";
 
-print <<<_HTML_
+  $user = $_POST['user'];
+
+  print $user;
+} else {
+  // иначе вывести на экран сому форму
+  print <<<_HTML_
 <form method="post" action="$_SERVER[PHP_SELF]">
 Ваше имя <input type="text" name="user"><br>
 <button type="submit">Отправить</button> 
 </form>
 _HTML_;
+}
 
