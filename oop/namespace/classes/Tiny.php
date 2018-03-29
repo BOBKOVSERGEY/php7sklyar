868203f775444e669ad771dcc25dd7097cffd6f1
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-03-29 10:14:03 +0300

    Namespace

---
+++ oop/namespace/classes/Tiny.php
@@ -0,0 +1,10 @@
+<?php
+namespace Tiny;
+
+class Fruit
+{
+  public static function much($bite)
+  {
+    print "Here is a tiny much of $bite";
+  }
+}
---
+++ oop/namespace/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/classes/Tiny.php';
+
+use Tiny\Fruit;
+
+Fruit::much("banana");
